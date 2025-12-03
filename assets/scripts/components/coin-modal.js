bb3d5497cff2fb6ae396de14506dae6027e8dd4b
miller275 <norok.waihaha@gmail.com>
2025-12-03 20:45:53 +0200

    Delete assets directory

--- assets/scripts/components/coin-modal.js
+++
@@ -1,7 +0,0 @@
-import { loadCoinDetail } from '../api/coingecko.js';
-import { fmt } from '../utils/format.js';
-
-export function initCoinModal() {
-  // Здесь логика открытия модалки и заполнения данными
-  // (по аналогии с твоим монолитным кодом)
-}
--- assets/scripts/components/pagination.js
+++
@@ -1,3 +0,0 @@
-export function initPagination() {
-  // Логика переключения страниц
-}
