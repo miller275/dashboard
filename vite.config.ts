2cf5605afc555cf942dfb95c26efeed2473d7c1d
miller275 <norok.waihaha@gmail.com>
2025-12-06 19:35:50 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// IMPORTANT: for GitHub Pages change `base` to `/<REPO_NAME>/`
-// or keep './' if you deploy just the `dist` folder.
 export default defineConfig({
+  base: '/dashboard/',  // <-- важно!
   plugins: [react()],
-  base: './',
   build: {
-    outDir: 'dist',
-    sourcemap: false
+    outDir: 'dist'
   }
 });
